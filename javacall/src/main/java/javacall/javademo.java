84033e5e5d07dab0f4ddbcf84d0e4520cd0dc4e1
Tejasreerachuri <tejasree.rachuri@gmail.com>
2022-01-03 16:29:18 +0530

    first commit

---
+++ javacall/src/main/java/javacall/javademo.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package javacall;
+
+/**
+ * @author Teja sree
+ *
+ */
+public class javademo {
+	 public static String getwelcomemessage() {
+		  return "Hello and welcme tejasree";
+	 }
+}
